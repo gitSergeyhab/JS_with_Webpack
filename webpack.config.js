bec7b9f0ac7f30b117d45338a8288fd676a5ee0c
Sergey <redmoloch@yanadex.ru>
2021-03-16 13:55:17 +0300

    initial commit -> added webpack

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// const path = require('path');
+const HTMLlPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: ['./src/index.js'],
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: __dirname + '/dist'
+    },
+    plugins: [
+        new HTMLlPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    resolve: {
+        extensions: ['.js']
+    }
+}
