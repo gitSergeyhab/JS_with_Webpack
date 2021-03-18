b1d3563a2386ea1ce43d9990ca37a5c33e18db61
Sergey <redmoloch@yandex.ru>
2021-03-18 17:41:01 +0300

    added form creator

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,9 +1,29 @@
 import {Component} from '../core/component';
+import {Form} from '../core/form'
 
 class CreateCompomemt extends Component {
     constructor(id) {
         super(id)
     }
+
+    init() {
+        this.$el.addEventListener('submit', submitHandler.bind(this))
+
+        this.form = new Form(this.$el, {
+            title: [],
+            fulltext: []
+        });
+    }
+}
+
+function submitHandler(evt) {
+    evt.preventDefault();
+
+    const formData = {
+        type: this.$el.type.value,
+        ...this.form.value()
+    }
+    console.log(formData)
 }
 
 export {CreateCompomemt};
---
+++ src/core/form.js
@@ -0,0 +1,16 @@
+class Form {
+    constructor(form, controls) {
+        this.form = form
+        this.controls = controls
+    }
+
+    value() {
+        const value = {};
+        Object.keys(this.controls).forEach(control => {
+            value[control] = this.form[control].value
+        })
+        return value;
+    }
+}
+
+export {Form}
