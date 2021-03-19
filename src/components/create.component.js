6f4cfeee5a90aa5d2363411665a842bd350e6b8d
Sergey <redmoloch@yanadex.ru>
2021-03-19 14:06:01 +0300

    added validators

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,5 +1,6 @@
 import {Component} from '../core/component';
-import {Form} from '../core/form'
+import {Form} from '../core/form';
+import {Validators} from '../core/validators'
 
 class CreateCompomemt extends Component {
     constructor(id) {
@@ -10,8 +11,8 @@ class CreateCompomemt extends Component {
         this.$el.addEventListener('submit', submitHandler.bind(this))
 
         this.form = new Form(this.$el, {
-            title: [],
-            fulltext: []
+            title: [Validators.required],
+            fulltext: [Validators.required, Validators.minLength(10)]
         });
     }
 }
@@ -19,11 +20,17 @@ class CreateCompomemt extends Component {
 function submitHandler(evt) {
     evt.preventDefault();
 
-    const formData = {
-        type: this.$el.type.value,
-        ...this.form.value()
+    if(this.form.isValid()) {
+
+        const formData = {
+            type: this.$el.type.value,
+            ...this.form.value()
+        }
+        console.log(formData)
+    } else {
+        console.warn('forms are not valid!')
     }
-    console.log(formData)
+
 }
 
 export {CreateCompomemt};
--- src/core/form.js
+++ src/core/form.js
@@ -11,6 +11,21 @@ class Form {
         })
         return value;
     }
+
+    isValid() {
+        let isFormValid = true;
+        Object.keys(this.controls).forEach(control => {
+            const validators = this.controls[control];
+            // let isValid = true;
+            validators.forEach(val => {
+                // isValid = val(this.form[control].value) && isValid;
+                isFormValid = val(this.form[control].value) && isFormValid;
+            })
+            // isFormValid = isFormValid && isValid
+        })
+        return isFormValid
+
+    }
 }
 
 export {Form}
